97c23ab1249ec39b368fa130945bbb6788a93275
Abdo Gamal Kashkoush <160290297+AbdoGKash@users.noreply.github.com>
2025-04-07 03:49:32 +0200

    Update api_constants.dart

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
-  static const String apiBaseUrl = "https://vcare.integration25.com/api/";
+  static const String apiBaseUrl = "";
   static const String login = "auth/login";
   static const String signup = "auth/register";
   // =============================== api home =================================== //
-  static const String homeApiBaseUrl = "https://m0rgk.wiremockapi.cloud/";
+  static const String homeApiBaseUrl = "";
   static const String home = "home";
 }
